c9a30978a3ff63a20fd2a9bbe113302148e31ada
CameronTheDeveloper <cameronposey@outlook.com>
2023-04-11 00:01:13 -0700

    Add buttons HTML div, append buttons, edit style

--- src/index.js
+++ src/index.js
@@ -3,14 +3,15 @@ import { displayWelcome } from './welcome';
 import { displayContact } from './contact';
 
 const content = document.querySelector('#content');
+const buttons = document.querySelector('#buttons');
 const welcomeButton = document.createElement('button');
 const contactButton = document.createElement('button');
 
 welcomeButton.innerHTML = 'Home';
 contactButton.innerHTML = 'Contact Info';
 
-content.appendChild(welcomeButton);
-content.appendChild(contactButton);
+buttons.appendChild(welcomeButton);
+buttons.appendChild(contactButton);
 displayWelcome(content);
 
 welcomeButton.addEventListener('click', () => {
