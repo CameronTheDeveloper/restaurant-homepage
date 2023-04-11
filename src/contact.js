f9017b0f960a4282c964e13d6cc8278d2de42042
CameronTheDeveloper <cameronposey@outlook.com>
2023-04-11 01:49:45 -0700

    Edit contact info, alignment

--- src/contact.js
+++ src/contact.js
@@ -3,19 +3,19 @@ import './styles/contact.css';
 const displayContact = (content) => {
 
     content.innerHTML = '';
-    const ownerNameDiv = document.createElement('div');
+    const nameDiv = document.createElement('div');
     const PhoneNumDiv = document.createElement('div');
     const addressDiv = document.createElement('div');
 
-    ownerNameDiv.classList.add('owner-name');
+    nameDiv.classList.add('owner-name');
     PhoneNumDiv.classList.add('phone-number');
     addressDiv.classList.add('address');
 
-    content.appendChild(ownerNameDiv);
+    content.appendChild(nameDiv);
     content.appendChild(PhoneNumDiv);
     content.appendChild(addressDiv);
 
-    ownerNameDiv.innerHTML = 'First M. Last ';
+    nameDiv.innerHTML = 'Nature Restaurant';
     PhoneNumDiv.innerHTML = '(000)-000-0000';
     addressDiv.innerHTML = '123 Main Street, Suite 456 Meadowbrook, CA 98765';
 
