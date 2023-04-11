6f6448dca176475ece2ad9b996c45697f7d6355c
CameronTheDeveloper <cameronposey@outlook.com>
2023-04-10 23:23:45 -0700

    Add welcome, contact tab buttons
    Click these buttons to change the content.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
+import './styles/index.css';
 import { displayWelcome } from './welcome';
+import { displayContact } from './contact';
 
+const content = document.querySelector('#content');
+const welcomeButton = document.createElement('button');
+const contactButton = document.createElement('button');
+
+welcomeButton.innerHTML = 'Home';
+contactButton.innerHTML = 'Contact Info';
+
+content.appendChild(welcomeButton);
+content.appendChild(contactButton);
 displayWelcome();
+
+welcomeButton.addEventListener('click', () => {
+    content.innerHTML = '';
+    displayWelcome();
+});
+
+contactButton.addEventListener('click', () => {
+    content.innerHTML = '';
+    displayContact();
+});
