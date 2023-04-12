d903c34afb280f7f6328373a37a5a9efdd0894a1
CameronTheDeveloper <cameronposey@outlook.com>
2023-04-11 23:50:18 -0700

    Add Menu button, edit buttons style

--- src/index.js
+++ src/index.js
@@ -5,13 +5,18 @@ import { displayContact } from './contact';
 const content = document.querySelector('#content');
 const buttons = document.querySelector('#buttons');
 const welcomeButton = document.createElement('button');
+const menuButton = document.createElement('button');
 const contactButton = document.createElement('button');
 
+
 welcomeButton.innerHTML = 'Home';
+menuButton.innerHTML = 'Menu';
 contactButton.innerHTML = 'Contact Info';
 
 buttons.appendChild(welcomeButton);
+buttons.appendChild(menuButton);
 buttons.appendChild(contactButton);
+
 displayWelcome(content);
 
 welcomeButton.addEventListener('click', () => {
