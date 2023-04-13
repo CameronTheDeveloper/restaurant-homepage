5642757bcd7afa29b223f736b4e7b00c54fdfba8
CameronTheDeveloper <cameronposey@outlook.com>
2023-04-12 20:39:43 -0700

    Format menu.js specials div

--- src/menu.js
+++ src/menu.js
@@ -1,20 +1,25 @@
 function createFood(foodName, foodPrice) {
-    const displayFood = (content) => {
-        const foodDiv = document.createElement('div');
-        foodDiv.innerHTML = foodName + ' -- $' + foodPrice;
-        content.appendChild(foodDiv);
+    const displayFood = (specialsDiv) => {
+        const foodItemDiv = document.createElement('div');
+        foodItemDiv.classList.add('food-item');
+        foodItemDiv.innerHTML = foodName + ' -- $' + foodPrice;
+        specialsDiv.appendChild(foodItemDiv);
     };
     return { displayFood };
 }
 
 const displayMenu = (content) => {
+    const specialsDiv = document.createElement('div');
     const pizza = createFood('Nature Pizza', 12.99);
     const burger = createFood('Nature Burger', 11.99);
     const specialsAr = [pizza, burger];
-    content.innerHTML = '';
 
+    content.innerHTML = '';
+    specialsDiv.classList.add('specials');
+    content.appendChild(specialsDiv);
+    /* Display specials loop */
     for (let i = 0; i < specialsAr.length; i++) {
-        specialsAr[i].displayFood(content);
+        specialsAr[i].displayFood(specialsDiv);
     }
 
 };
