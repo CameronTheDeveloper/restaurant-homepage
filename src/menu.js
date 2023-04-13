31b5d1becfd133f2c8e9bc6d9e828fdc457b6141
CameronTheDeveloper <cameronposey@outlook.com>
2023-04-12 20:55:58 -0700

    Style menu.js with menu.css, add specials title

--- src/menu.js
+++ src/menu.js
@@ -1,3 +1,5 @@
+import './styles/menu.css';
+
 function createFood(foodName, foodPrice) {
     const displayFood = (specialsDiv) => {
         const foodItemDiv = document.createElement('div');
@@ -10,12 +12,18 @@ function createFood(foodName, foodPrice) {
 
 const displayMenu = (content) => {
     const specialsDiv = document.createElement('div');
+    const specialsTitle = document.createElement('div');
     const pizza = createFood('Nature Pizza', 12.99);
     const burger = createFood('Nature Burger', 11.99);
     const specialsAr = [pizza, burger];
 
     content.innerHTML = '';
+    specialsTitle.innerHTML = 'Kitchen Specials';
+
     specialsDiv.classList.add('specials');
+    specialsTitle.classList.add('title', 'specials');
+
+    specialsDiv.appendChild(specialsTitle);
     content.appendChild(specialsDiv);
     /* Display specials loop */
     for (let i = 0; i < specialsAr.length; i++) {
