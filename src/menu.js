63af25f7906a8a1a330e6aafbcfbfd18c6c74e07
CameronTheDeveloper <cameronposey@outlook.com>
2023-04-13 18:25:06 -0700

    Add menu.js lunch, align menu

--- src/menu.js
+++ src/menu.js
@@ -2,7 +2,12 @@ import './styles/menu.css';
 
 const pizza = createFood('Nature Pizza', 12.99);
 const burger = createFood('Nature Burger', 11.99);
-const specialsAr = [pizza, burger];
+let specialsAr = [pizza, burger];
+
+const pasta = createFood('Nature Pasta', 12.99);
+const veggieBurger = createFood('Veggie Burger', 11.99);
+const grillCheese = createFood('Grilled Cheese Sandwich', 6.99);
+let lunchAr = [pasta, veggieBurger, grillCheese];
 
 function createFood(foodName, foodPrice) {
     const displayFood = (specialsDiv) => {
@@ -18,17 +23,31 @@ const displayMenu = (content) => {
     const specialsDiv = document.createElement('div');
     const specialsTitle = document.createElement('div');
 
+    const lunchDiv = document.createElement('div');
+    const lunchTitle = document.createElement('div');
+
     content.innerHTML = '';
     specialsTitle.innerHTML = 'Kitchen Specials';
+    lunchTitle.innerHTML = 'Lunch Crafts';
+
 
     specialsDiv.classList.add('specials');
     specialsTitle.classList.add('title', 'specials');
+    lunchDiv.classList.add('lunch');
+    lunchTitle.classList.add('title', 'lunch');
 
     specialsDiv.appendChild(specialsTitle);
     content.appendChild(specialsDiv);
+    lunchDiv.appendChild(lunchTitle);
+    content.appendChild(lunchDiv);
+
     /* Display specials loop */
     for (let i = 0; i < specialsAr.length; i++) {
         specialsAr[i].displayFood(specialsDiv);
+    }
+
+    for (let j = 0; j < lunchAr.length; j++) {
+        lunchAr[j].displayFood(lunchDiv);
     }
 
 };
